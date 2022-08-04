e148a3ad1e774de6d3d2c1683e949b1947bb7486
Daryana13 <110623251+Daryana13@users.noreply.github.com>
2022-08-04 19:49:30 +0300

    Delete Дом.задание directory

--- Дом.задание/ДР/DZ_1_Bondarev_Alexandr.py
+++
@@ -1,9 +0,0 @@
-import random
-r = random.randint(100, 999)
-r = str(r)
-print("Вывод случайного 3-х значного: ", r)
-a = int(r[0])
-b = int(r[1])
-c = int(r[2])
-
-print("Сумма цифр числа:", a + b + c)
--- Дом.задание/Дз4.1ДарьяБруцая.py
+++
@@ -1,4 +0,0 @@
-for i in range(1,101,2):
-    for a in range(1,101,2):
-        print(i*a, '', end= '')
-print()
